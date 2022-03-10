665c57f2718113a98293aab44184d12a73226919
Nathan Prat <nathan.prat@gmail.com>
2022-03-10 17:18:47 +0100

    make the IPFS address configurable by flag

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,16 @@
+use clap::Parser;
 use tonic::transport::Server;
 
 mod circuits_routes;
+
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    /// Where to reach the IPFS node
+    #[clap(long, default_value = "/ip4/127.0.0.1/tcp/5001")]
+    ipfs_server_multiaddr: String,
+}
 
 // TODO DRY server creation with the tests
 // cf https://github.com/hyperium/tonic/blob/4b0ece6d2854af088fbc1bdb55c2cdd19ec9bb92/tonic-web/tests/integration/tests/grpc.rs#L113
@@ -8,13 +18,13 @@ mod circuits_routes;
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     tracing_subscriber::fmt::init();
 
+    let args = Args::parse();
+
     // TODO configurable port
     let addr = "0.0.0.0:3000".parse().unwrap();
-    let ipfs_server_port = 5001;
-    let ipfs_server_multiaddr = format!("/ip4/127.0.0.1/tcp/{}", ipfs_server_port);
 
     let circuits_api = circuits_routes::SkcdApiServerImpl {
-        ipfs_server_multiaddr: ipfs_server_multiaddr,
+        ipfs_server_multiaddr: args.ipfs_server_multiaddr,
     };
     let circuits_api =
         circuits_routes::interstellarpbapicircuits::skcd_api_server::SkcdApiServer::new(
